912fc1cc03a75236de2162cb69aecaf7f3c285fd
GitFahim350 <fahimaust609@gmail.com>
2024-09-05 11:12:07 +0600

    project initialized

---
+++ src/main/java/com/finanace_management_system/personal_finance_management_system/PersonalFinanceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.finanace_management_system.personal_finance_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finanace_management_system/personal_finance_management_system/PersonalFinanceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finanace_management_system.personal_finance_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
